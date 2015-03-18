300c9d0edf47f7c178688bcd10c5a7b882957285
rennishj <rennishj@gmail.com>
2015-03-18 10:06:37 -0400

    Initial Raven Db check in

---
+++ RavenDb.Mvc/RavenDb.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RavenDb.Web.MvcApplication" Language="C#" %>
---
+++ RavenDb.Mvc/RavenDb.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RavenDb.Web.Startup))]
+namespace RavenDb.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
